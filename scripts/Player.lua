e12fce7b625118c4e12801c4afbfce99fa6b20c7
Marco Alanen <marco@ormgas.com>
2012-10-05 10:58:11 +0200

    Change player reference point from center to top left of the object.

--- scripts/Player.lua
+++ scripts/Player.lua
@@ -2,7 +2,7 @@ module(..., package.seeall)
 
 function newPlayer(x, y, w, h)
    local entity = display.newRect(x, y, w, h)
-   --entity:setReferencePoint(display.centerReferencePoint)
+   entity:setReferencePoint(display.TopLeftReferencePoint)
    entity:setFillColor(255)
    
    return entity
